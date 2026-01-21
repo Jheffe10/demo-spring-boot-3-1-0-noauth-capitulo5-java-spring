ecacd238a4ff43f2e45c0714bb51eff81d2dd71e
Jheffe10 <jheffeguerreiro@yahoo.com.br>
2026-01-21 14:53:05 -0300

    BCrypt password encoder

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -4,10 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication {//implements CommandLineRunner{ //Para testar a cryptografia da senha
+	
+	//Para testar a cryptografia da senha
+//	@Autowired //injeção de 
+//	private PasswordEncoder passwordEncoder; //como coloquei o componente @Bean no método da classe SecurityConfig, já cria uma instância do BCrypt
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	//Para testar a cryptografia da senha
+//	@Override
+//	public void run(String... args) throws Exception {
+//		//testar a criptografia da senha
+//		System.out.println("Encode= " +passwordEncoder.encode("123456")); //como parâmetro passo a senha para codificação
+//				//gerou esse hash: $2a$10$rCoTx58/8N0l1xG38EQUxOAChv7PgoAuju5qB4CbBXsINvAfDln4m
+//				//No banco de dados salvo esse hash. na hora que o usuário vai logar e passa a sneha, comparo a senha com esse hash
+//		//o Spring Security faz a comparação conforme abaixo:
+//		boolean result = passwordEncoder.matches("123456", "$2a$10$rCoTx58/8N0l1xG38EQUxOAChv7PgoAuju5qB4CbBXsINvAfDln4m");
+//		System.out.println("Result= " +result);
+//	}
 
 }
--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,14 +3,27 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-//Como adicionei as dependências do security no projeto (no arquivo .pom) já não consigo mais acessar os endpoints
-//Aí posso liberar provisoriamente os endpoints através da criação dessa classe
-//Com isso não ocorre mais o erro 401 - falta de autorização e os endpoints ficam liberados
+
 @Configuration
 public class SecurityConfig {
 
+	//Nessa classe de configuração defino componentes na forma de métodos.
+	//Dentro dessa classe configuro componentes: @Bean...
+	
+	//Configurar a ciptografia da senha (password encode)
+	@Bean
+	public PasswordEncoder getPassowrdEncoder() { //PassworEncoder é a interface, um tipo genérico. Assim posso utilizar qualquer implementação dessa interface. Uma delas é o BCrypt
+		return new BCryptPasswordEncoder(); //Só retornar um new que funciona, instancia o objeto e como tem o @Bean o objeto vira
+								//um componente que pode ser injetado (através do @Autowired) em outros lugares
+	}
+	
+	//Como adicionei as dependências do security no projeto (no arquivo .pom) já não consigo mais acessar os endpoints
+	//Aí posso liberar provisoriamente os endpoints através da criação dessa classe
+	//Com isso não ocorre mais o erro 401 - falta de autorização e os endpoints ficam liberados
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable());//desabilitando ataques do tipo csrf
